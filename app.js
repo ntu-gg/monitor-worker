9d1683cb20c61f3773a0db39af379b4e18837fe4
shouko <imshouko@gmail.com>
2016-05-30 16:44:45 +0800

    add interaction with API

--- app.js
+++ app.js
@@ -1,43 +1,67 @@
 var rp = require('request-promise');
 var schedule = require('node-schedule');
+var config = require('./config');
 
 var rule = new schedule.RecurrenceRule();
 rule.second = 0;
 
-var urls = [
-  'https://my.ntu.edu.tw/',
-  'https://ceiba.ntu.edu.tw/',
-  'https://mail.ntu.edu.tw/',
-  'https://ifsel3.aca.ntu.edu.tw/', // 成績查詢
-  'https://if163.aca.ntu.edu.tw/eportfolio/', // ePo
-  'https://if192.aca.ntu.edu.tw/', // 選課系統
-  'https://if177.aca.ntu.edu.tw/', // 選課系統 2
-  'https://nol.ntu.edu.tw/nol/guest/index.php', // 課程網
-  'https://nol2.aca.ntu.edu.tw/nol/guest/index.php', // 課程網 2
-  'https://www.space.ntu.edu.tw/navigate/' // NTU Space
-];
+var jobs = [];
 
 var headers = {
   'Accept': '*/*; q=0.01',
   'User-Agent': 'Mozilla/5.0 (Linux; U; Android 4.2.2; en-us; Google Nexus 4 - 4.2.2 - API 17 - 768x1280 Build/JDQ39E) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30'
 };
 
+var report = function(job_id, stime, rt, statusCode, msg) {
+  return rp({
+    url: config.api.endpoint + 'report',
+    method: 'POST',
+    json: true,
+    auth: {
+      user: config.api.client_id,
+      pass: config.api.key
+    },
+    body: {
+      job_id: job_id,
+      slot: Math.round(stime/1000/60),
+      stime: stime,
+      rt: rt,
+      statusCode: statusCode,
+      msg: msg
+    }
+  });
+};
+
+var fetch = function() {
+  return rp({
+    url: config.api.endpoint + 'jobs',
+    json: true,
+  }).then(function(res) {
+    jobs = res;
+  });
+}
+
 var monitor = function() {
-  urls.forEach(function(url) {
+  jobs.forEach(function(entry) {
     new Promise(function() {
       var stime = new Date();
       rp({
-        url: url,
+        url: entry.url,
         method: 'GET',
         headers: headers,
         resolveWithFullResponse: true,
+        timeout: 30000,
         simple: false
       }).then(function(res) {
         var rt = new Date() - stime;
-        console.log(Math.round(stime/1000/60), stime, url, res.statusCode, rt + 'ms');
+        report(entry.id, stime, rt, res.statusCode, '');
+      }).catch(function(err) {
+        report(entry.id, stime, -1, -1, err.error.code);
       });
     });
   });
 };
 
-var j = schedule.scheduleJob(rule, monitor);
+fetch().then(funciton() {
+  var j = schedule.scheduleJob(rule, monitor);
+});
---
+++ config.example.js
@@ -0,0 +1,7 @@
+module.exports = {
+  api: {
+    endpoint: 'http://example.com/',
+    client_id: '',
+    key: ''
+  }
+};
